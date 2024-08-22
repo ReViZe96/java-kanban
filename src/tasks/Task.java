e1ef5dca3532bcfa885828beb2665c2323c42ada
Artem Kasyanov <mr.revize.com@gmail.com>
2024-08-23 01:32:28 +0300

    tracker 0.7.2: functional of TZ from sprint 9: code style checking

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -14,8 +14,6 @@ public class Task implements Comparable<Task> {
     protected Duration duration;
     protected LocalDateTime startTime;
 
-    public Task() {}
-
     public Task(String name, String description) {
         this.name = name;
         this.description = description;
